f272d4f0935ca2159a14c34a5112d368b5d7f548
yatoyun <kaneko.yuta0813@gmail.com>
2024-09-15 15:34:17 +0900

    feat: presenterの実装

---
+++ api/interface/presenter/user.go
@@ -0,0 +1,22 @@
+package presenter
+
+import "github.com/yatoyun/todo-app/api/domain/application/usecase"
+
+type UserPresenter struct {
+}
+
+func NewUserPresenter() usecase.UserOutputPortInterface {
+	return &UserPresenter{}
+}
+
+func (p *UserPresenter) Convert(outputData usecase.UserOutputData) (*usecase.UserResponse, error) {
+	return &usecase.UserResponse{
+		ID:        outputData.ID,
+		Name:      outputData.Name,
+		Email:     outputData.Email,
+		Auth0ID:   outputData.Auth0ID,
+		Role:      outputData.Role,
+		CreatedAt: outputData.CreatedAt,
+		UpdatedAt: outputData.UpdatedAt,
+	}, nil
+}
