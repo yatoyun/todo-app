d2de2dfca007d393f4e228769d23da174ef2a48d
yatoyun <kaneko.yuta0813@gmail.com>
2024-09-18 19:07:16 +0900

    fix: 構造体tagを追加

--- api/domain/entity/user.go
+++ api/domain/entity/user.go
@@ -5,11 +5,11 @@ import (
 )
 
 type User struct {
-	ID string `json:"id"` // UUIDを想定
-	Name string `json:"name"`
-	Email string `json:"email"`
-	Auth0ID string `json:"auth0_id"`
-	Role string `json:"role"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        string    `json:"id" db:"id"` // UUIDを想定
+	Name      string    `json:"name" db:"name"`
+	Email     string    `json:"email" db:"email"`
+	Auth0ID   string    `json:"auth0_id" db:"auth0_id"`
+	Role      string    `json:"role" db:"role"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
 }
